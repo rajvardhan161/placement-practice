857c8d094408a9f6532231fa2e65295198922a8b
rajvardhan161 <143264685+rajvardhan161@users.noreply.github.com>
2026-01-30 15:29:25 +0530

    Add function to sort array by parity
    Implement sorting of array by parity using a two-pointer approach.

---
+++ 905-LC.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int n = nums.size();
+        int i=0;
+        for(int j =0;j<n;j++){
+            if(nums[j]%2==0){
+                swap(nums[j],nums[i]);
+                i++;
+            }
+        }
+    return nums;
+    }
+};
